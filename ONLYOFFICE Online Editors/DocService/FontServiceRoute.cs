ac46169bd0425eb0082fd93bdd6b78d1689f1a08
apozhidaev <alexander.pozhidaev@gmail.com>
2014-09-07 01:11:37 +0400

    TeamLab editor '1

---
+++ ONLYOFFICE Online Editors/DocService/FontServiceRoute.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+public class FontServiceRoute : IRouteHandler
+{
+    public IHttpHandler GetHttpHandler(RequestContext requestContext)
+    {
+        return new FontService(requestContext);
+    }
+}
---
+++ ONLYOFFICE Online Editors/DocService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="ASP.global_asax" %>
